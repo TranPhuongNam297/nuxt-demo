3e314f327ad0011798e8bb9c97e7a68a2e02d178
Nam Tran <namtran0108642@gmail.com>
2024-11-07 08:48:35 +0700

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+      <Header />
+      <section>
+        <h1>Welcome to Yen Market</h1>
+        <p>Explore our exclusive products!</p>
+      </section>
+      <Footer />
+    </div>
+</template>
+
+<script>
+  import Header from '~/components/Header.vue';
+  import Footer from '~/components/Footer.vue';
+  
+  export default {
+    components: { Header, Footer },
+  };
+  </script>
+  
+  <style scoped>
+
+  </style>
